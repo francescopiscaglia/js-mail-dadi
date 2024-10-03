56782a786aa2ae5c4289339c03a4d81abec2c25f
francescopiscaglia <piscagliafrancesco@gmail.com>
2024-10-03 14:13:26 +0200

    add base comment

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,22 @@
-console.log("Hello world");
-
-// 0. Strumenti 
-
 // 1. Preparazione
+// creo una variabile user_email per la mail dell'utente
+
 
 // 2. Analisi dati
+// dichiaro la variabile user_email con un prompt per chiedere all'utente
+
 
 // 3. Elaborazione codice
+// conditional block
+// verifico se user_email è nella lista che può accedere al gioco
+// stampo il messaggio che può accedere
+
+// altrimenti non può accedere
+// stampo il messaggio che non può accedere al gioco
+
+
+// array per i numeri da 1 a 6
+// prompt
+
 
 // 4. Output
