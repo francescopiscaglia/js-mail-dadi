efa405f6a8c61534f3e42f9bc85c647a33d29c02
francescopiscaglia <piscagliafrancesco@gmail.com>
2024-10-03 14:49:45 +0200

    add a verification progress for @ char in email and .toLowerCase method

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,7 +5,7 @@ let user_email;
 
 // 2. Analisi dati
 // dichiaro la variabile user_email con un prompt per chiedere all'utente
-user_email = prompt("Ciao, inserisci la tua email per accedere al gioco");
+user_email = prompt("Ciao, inserisci la tua email per accedere al gioco (tutto in minuscolo").toLowerCase();
 
 // stampo la email inserita dall'utente
 console.log(user_email);
@@ -14,15 +14,25 @@ console.log(user_email);
 let verify_email = ["si@gmail.com", "access@gmail.com"];
 
 // 3. Elaborazione codice
-// verifico se user_email è nella lista che può accedere al gioco
-if (verify_email.includes(user_email)) {
-    // stampo il messaggio che può accedere
-    console.log("Puoi accedere al gioco");
+// verifico se la email contiene il carattere @ per validare che sia una mail
+if (user_email.includes("@")) {
+    // stampo che è valida
+    console.log("Email valida");
 
-    // altrimenti non può accedere
+    // verifico se user_email è nella lista che può accedere al gioco
+    if (verify_email.includes(user_email)) {
+        // stampo il messaggio che può accedere
+        console.log("Puoi accedere al gioco");
+    
+        // altrimenti non può accedere
+    } else {
+        // stampo il messaggio che non può accedere al gioco
+        console.log("Non puoi accedere al gioco");
+    }
+
 } else {
-    // stampo il messaggio che non può accedere al gioco
-    console.log("Non puoi accedere al gioco");
+    //stampo che non è valida
+    console.log("Email non valida, riprova");
 }
 
 
